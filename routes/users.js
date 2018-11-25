6376a827675b446bd48e7be0d2797fc6b3a8778b
Gaurang Agarwal <er.gaurangagarwal@yahoo.com>
2018-11-25 08:25:35 +0530

    Add registeration logic

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,44 @@
-const express = require('express');
+import User from "../models/User";
 
-const router= express.Router();
+const express = require("express");
+const gravatar = require("gravatar");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
 
+router.get("/test", (req, res) => res.json({ msg: "test users" }));
 
-router.get("/test", (req,res)=> res.json({"msg":"test users"}));
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user)
+        return res.status(400).json({ message: "Email Already Exists" });
+
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+        protocol: "http"
+      });
+
+      const newUser = new User({
+        email: req.body.email,
+        name: req.body.name,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.status(200).json(newUser))
+            .catch(err => res.status(400));
+        });
+      });
+    })
+    .catch(error => console.log(error));
+});
 
 export default router;
