dc3385815f2061d716814a7239dd1ca33c28cdf3
Gaurang Agarwal <er.gaurangagarwal@yahoo.com>
2018-11-25 14:17:42 +0530

    Add JWS token in the login response

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 import User from "../models/User";
+import { SECRET } from "../config";
 
 const express = require("express");
 const gravatar = require("gravatar");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.get("/test", (req, res) => res.json({ msg: "test users" }));
 
@@ -42,20 +44,27 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  User.findOne({ email: req.body.email }).then( user => {
-  if (!user) return res.status(404).json({ msg: "Email ID doesnot exists" });
-  console.log(user.email,user.password);
+  User.findOne({ email: req.body.email }).then(user => {
+    if (!user) return res.status(404).json({ msg: "Email ID doesnot exists" });
+    console.log(user.email, user.password);
 
-  const password= user.password;
-  bcrypt.compare(req.body.password,password).then(isMatched => {
-      if(isMatched){
-        res.status(200).json({msg: 'Login Successful'});
+    const password = user.password;
+    bcrypt.compare(req.body.password, password).then(isMatched => {
+      if (isMatched) {
+        const payLoad = { id: user.id, name: user.name, email: user.email };
+        jwt.sign(
+            payLoad, 
+            SECRET, 
+            { expiresIn: 3600 }, 
+            (err, token) => {
+          res.json({ success: true, token: "Bearer " + token });
+        });
+
         return;
-        }
-        res.status(400).json({msg: 'Invalid Password'});
-
+      }
+      res.status(400).json({ msg: "Invalid Password" });
+    });
   });
-});
 });
 
 export default router;
